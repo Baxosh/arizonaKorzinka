bffcbb351e2b4b158ed8e3b9cea0f69f0380515d
Bakhodir <bakhodir.2000.19@gmail.com>
2022-09-20 16:32:53 +0500

    Add folder

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import Router from './Router'
+
+import './static/css/style.css'
+import 'bulma/css/bulma.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Router />)
